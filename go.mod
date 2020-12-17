ccf96a1f33baca51d43501bd290a1ba9ae32e648
dojin <dojinkim119@gmail.com>
2020-12-17 23:14:54 +0900

    :sparkles: implement simple gRPC server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dojinkimm/go-example
+
+go 1.14
+
+require google.golang.org/grpc v1.34.0
---
+++ simple/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const portNumber = "9000"
+
+func main(){
+	lis, err := net.Listen("tcp", ":" + portNumber)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	log.Printf("start gRPC server on %s port", portNumber)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
