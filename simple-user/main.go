c1fc841fc3979ab72a8d7066128d5b9be06bd4e5
dojin <dojinkim119@gmail.com>
2020-12-20 12:00:46 +0900

    :sparkles: Implement simple-user service gRPC server

--- simple-user/main.go
+++ simple-user/main.go
@@ -1,1 +1,64 @@
-package simple_user
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	"github.com/dojinkimm/go-example/data"
+	pb "github.com/dojinkimm/go-example/user"
+)
+
+const portNumber = "9000"
+
+type userServer struct {
+	pb.UnimplementedUserServer
+}
+
+// GetUser returns user message by user_id
+func (s *userServer) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.GetUserResponse, error) {
+	userID := req.UserId
+
+	var userMessage *pb.UserMessage
+	for _, u := range data.UserData {
+		if u.UserId != userID {
+			continue
+		}
+		userMessage = u
+		break
+	}
+
+	return &pb.GetUserResponse{
+		UserMessage: userMessage,
+	}, nil
+}
+
+// ListUsers returns all user messages
+func (s *userServer) ListUsers(ctx context.Context, req *pb.ListUsersRequest) (*pb.ListUsersResponse, error) {
+	userMessages := make([]*pb.UserMessage, len(data.UserData))
+	for i, u := range data.UserData {
+		userMessages[i] = u
+	}
+
+	return &pb.ListUsersResponse{
+		UserMessages: userMessages,
+	}, nil
+}
+
+func main(){
+	lis, err := net.Listen("tcp", ":" + portNumber)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterUserServer(grpcServer, &userServer{})
+
+	log.Printf("start gRPC server on %s port", portNumber)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
+
