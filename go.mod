bc4f96653ff6d275b85d20639202fad48263126b
dojin <dojinkim119@gmail.com>
2021-02-28 17:38:38 +0900

    :sparkles: Implement grpc-middleware logging logrus

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang/protobuf v1.4.3
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.1.0
+	github.com/sirupsen/logrus v1.8.0 // indirect
 	google.golang.org/genproto v0.0.0-20210106152847-07624b53cd92
 	google.golang.org/grpc v1.34.0
 	google.golang.org/protobuf v1.25.0
---
+++ grpc-middlewares/logging-logrus/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
+	"net"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+
+	"github.com/dojinkimm/go-grpc-example/data"
+	userpb "github.com/dojinkimm/go-grpc-example/protos/v1/user"
+)
+
+const portNumber = "9000"
+
+type userServer struct {
+	userpb.UserServer
+}
+
+// GetUser returns user message by user_id
+func (s *userServer) GetUser(ctx context.Context, req *userpb.GetUserRequest) (*userpb.GetUserResponse, error) {
+	userID := req.UserId
+
+	var userMessage *userpb.UserMessage
+	for _, u := range data.Users {
+		if u.UserId != userID {
+			continue
+		}
+		userMessage = u
+		break
+	}
+
+	if userMessage == nil {
+		return nil, status.Error(codes.NotFound, "user is not found")
+	}
+
+	return &userpb.GetUserResponse{
+		UserMessage: userMessage,
+	}, nil
+}
+
+// ListUsers returns all user messages
+func (s *userServer) ListUsers(ctx context.Context, req *userpb.ListUsersRequest) (*userpb.ListUsersResponse, error) {
+	userMessages := make([]*userpb.UserMessage, len(data.Users))
+	for i, u := range data.Users {
+		userMessages[i] = u
+	}
+
+	return &userpb.ListUsersResponse{
+		UserMessages: userMessages,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":"+portNumber)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	logrus.ErrorKey = "grpc.error"
+	logrusEntry := logrus.NewEntry(logrus.StandardLogger())
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_logrus.UnaryServerInterceptor(logrusEntry),
+		)),
+	)
+	userpb.RegisterUserServer(grpcServer, &userServer{})
+
+	log.Printf("start gRPC server on %s port", portNumber)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
