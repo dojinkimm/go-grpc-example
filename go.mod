c4379bb9b2edac9fcc299fd16141eb6641022a4b
dojin <dojinkim119@gmail.com>
2021-02-28 16:32:24 +0900

    :sparkles: Implement simple grpc-middleware

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.3
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.1.0
 	google.golang.org/genproto v0.0.0-20210106152847-07624b53cd92
 	google.golang.org/grpc v1.34.0
---
+++ simple-middleware/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	"log"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+
+	"github.com/dojinkimm/go-grpc-example/data"
+	userpb "github.com/dojinkimm/go-grpc-example/protos/v1/user"
+)
+
+const portNumber = "9000"
+
+type userServer struct {
+	userpb.UserServer
+}
+
+// GetUser returns user message by user_id
+func (s *userServer) GetUser(ctx context.Context, req *userpb.GetUserRequest) (*userpb.GetUserResponse, error) {
+	userID := req.UserId
+
+	var userMessage *userpb.UserMessage
+	for _, u := range data.Users {
+		if u.UserId != userID {
+			continue
+		}
+		userMessage = u
+		break
+	}
+
+	return &userpb.GetUserResponse{
+		UserMessage: userMessage,
+	}, nil
+}
+
+// ListUsers returns all user messages
+func (s *userServer) ListUsers(ctx context.Context, req *userpb.ListUsersRequest) (*userpb.ListUsersResponse, error) {
+	userMessages := make([]*userpb.UserMessage, len(data.Users))
+	for i, u := range data.Users {
+		userMessages[i] = u
+	}
+
+	return &userpb.ListUsersResponse{
+		UserMessages: userMessages,
+	}, nil
+}
+
+func customMiddleware() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (_ interface{}, err error) {
+		log.Print("Requested at:", time.Now())
+
+		resp, err := handler(ctx, req)
+		return resp, err
+	}
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":"+portNumber)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			customMiddleware(),
+			)),
+		)
+	userpb.RegisterUserServer(grpcServer, &userServer{})
+
+	log.Printf("start gRPC server on %s port", portNumber)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
